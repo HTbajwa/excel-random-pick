b221ee3936ad5929b5e0819910e161d2f7de4fcd
Hadia Tariq <tariqhadia12@gmail.com>
2025-10-26 08:44:51 -0700

    files added

---
+++ script.js
@@ -0,0 +1,6 @@
+// What you want to do	Doc section to read
+// Load library (CDN or npm)	Getting Started → Installation
+// Read Excel file	Parsing Workbooks
+// Convert to JSON	Utilities → sheet_to_json
+// Write / export Excel	Writing Workbooks
+// Save file in browser	I/O → Browser Download
